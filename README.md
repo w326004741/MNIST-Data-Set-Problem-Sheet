a1492eddc7c1baf6ce258c92ae62e0f3e09c549a
w326004741 <G00329371@gmit.ie>
2017-09-18 20:45:09 +0100

    git push

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Read-Digits-Image-Files-Problem-sheet
-hese problems relate to the famous MNIST data set.
+Solutions to the Read Digits Image Files problem sheet
+
+These files work with the famous MNIST data set. Please download the four data files from that website and place them (as gz files) in a subfolder called data/.
---
+++ rough-work.py
@@ -0,0 +1,14 @@
+
+# Adapted from: https://stackoverflow.com/questions/12902540/read-from-a-gzip-file-in-python
+# Also here: https://stackoverflow.com/questions/2872381/how-to-read-a-file-byte-by-byte-in-python-and-how-to-print-a-bytelist-as-a-binar
+
+import gzip
+
+f = gzip.open('/Users/weichenwang/Year4/Read-Digits-Image-Files-Problem-sheet/Data/t10k-images-idx3-ubyte.gz', 'rb')
+
+magic = f.read(4) #读取4个文件
+
+# this prints : b'\x00\x00\x08\x03'
+#In binary this is : 00000000 00000000 00001000 00000011
+
+print(int.from_bytes(magic,'big'))
